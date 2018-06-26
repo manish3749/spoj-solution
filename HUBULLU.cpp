6ec7e4c55083943ddf7c44f3a634895e8414d042
MANISH BHARDWAJ <manish0409365@gmail.com>
2018-06-26 13:48:45 +0530

    Create HUBULLU.cpp

---
+++ HUBULLU.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,p;
+        cin>>n>>p;
+        int wnr;
+        ll ansr=(n-n/2);
+        if(p==0)
+            cout<<"Airborne wins."<<endl;
+        else
+            cout<<"Pagfloyd wins."<<endl;
+    }
+}
