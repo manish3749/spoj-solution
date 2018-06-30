04f8f45bd8835eed7bf52267976b3a07570d205b
MANISH BHARDWAJ <manish0409365@gmail.com>
2018-06-30 08:51:15 +0530

    Create NGM.cpp

---
+++ NGM.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+    ll n;
+    cin>>n;
+    if(n%10!=0)
+    {
+        cout<<"1"<<endl;
+        cout<<n%10<<endl;
+    }
+    else
+        cout<<"2"<<endl;
+}
